47e834b5ce4b0fb1998268e5d880f2f729938b55
Arielstein <ariel.n.duarte14@gmail.com>
2025-03-14 23:59:22 -0600

    Actualización de la función

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ function agregarAmigo() {
     // validar la entrada
     let amigo = document.getElementById("amigo").value;
     if (amigo.trim() === '') {
-        console.log('Por favor, inserte un nombre.');
+        alert('Por favor, inserte un nombre.');
         return;
     }
 
+    //Agregar el nombre a la lista
     nombres.push(amigo);
+    console.log(nombres);
 
+    //Vaciar para ingresar nuevo nombre
+    document.getElementById("amigo").value = '';
 
 }
