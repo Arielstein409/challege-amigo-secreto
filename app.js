56086cb292eec9fd9aa564484b59919332434483
Arielstein <ariel.n.duarte14@gmail.com>
2025-03-14 22:09:32 -0600

    Construcción función

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 let nombres = [];
+
+function agregarAmigo() {
+
+    // validar la entrada
+    let amigo = document.getElementById("amigo").value;
+    if (amigo.trim() === '') {
+        console.log('Por favor, inserte un nombre.');
+        return;
+    }
+
+    nombres.push(amigo);
+
+
+}
